adf99660439639ae5d6c6b5a536170c653d6a725
XXZHH <1151509240@qq.com>
2019-01-10 11:28:47 +0800

    123

---
+++ lodash/xxzhh-lodash.js
@@ -0,0 +1,14 @@
+var xxzhh = {
+  chunk : function(ary, size = 1) {
+    var res = [];
+    var s = 0
+    var e = size
+    while (e < ary.length) {
+      res.push(ary.slice(s,size));
+      s = size;
+      e = size + size
+    }
+    res.push(ary.slice(s,ary.length))
+    return res;
+  }
+}
