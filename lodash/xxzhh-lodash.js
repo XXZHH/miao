78d6401bfd3806c2498a4a423efe319d53ea115a
XXZHH <1151509240@qq.com>
2019-01-10 17:14:31 +0800

    123

--- lodash/xxzhh-lodash.js
+++ lodash/xxzhh-lodash.js
@@ -1,18 +1,37 @@
 var xxzhh = {
-chunk : function chunk(ary, size = 1) {
+chunk : function(array, size = 1) {
     var res = [];
     var s = 0
     var e = size
-    while (e < ary.length) {
-      res.push(ary.slice(s,size));
+    while (e < array.length) {
+      res.push(array.slice(s,size));
       s = size;
       e = size + size
     }
-    res.push(ary.slice(s,ary.length))
+    res.push(ary.slice(s,array.length))
     return res;
   }
 
-  compact : function {
-    
+compact : function(array) {
+    res = [];
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] != false || array[i] != null || array[i] != 0|| array[i] != undefined || array[i] != NAN) {
+        res.push(array[i])
+      }
+    }
+    return res
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
