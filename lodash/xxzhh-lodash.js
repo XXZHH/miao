a0eb7de3112f1450245bd36c7f5090d0d91673d3
XXZHH <1151509240@qq.com>
2019-01-10 17:35:28 +0800

    123

--- lodash/xxzhh-lodash.js
+++ lodash/xxzhh-lodash.js
@@ -11,15 +11,7 @@ chunk : function(array, size = 1) {
     res.push(ary.slice(s,array.length))
     return res;
   },
-compact : function(array) {
-    res = [];
-    for (var i = 0; i < array.length; i++) {
-      if (array[i] != false || array[i] != null || array[i] != 0|| array[i] != undefined || array[i] != NAN) {
-        res.push(array[i])
-      }
-    }
-    return res
-  }
+
 
 
 
