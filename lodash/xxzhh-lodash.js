954942634400addc32dd7c62b1ae559c93df7af7
XXZHH <1151509240@qq.com>
2019-01-27 21:37:03 +0800

    123

--- lodash/xxzhh-lodash.js
+++ lodash/xxzhh-lodash.js
@@ -1,16 +1,53 @@
 var xxzhh = {
-chunk : function chunk(ary, size = 1) {
-    var res = [];
-    var s = 0
-    var e = size
-    while (e < ary.length) {
-      res.push(ary.slice(s,size));
+chunk: function chunk(array, size = 1) {
+    let res = [];
+    let s = 0
+    let e = size
+    while (e < array.length) {
+      res.push(array.slice(s,size));
       s = size;
       e = size + size
     }
-    res.push(ary.slice(s,ary.length))
+    res.push(array.slice(s,array.length))
     return res;
+  },
+
+compact: function compact(array) {
+    res = [];
+    for (let i = 0; i < array.length; i++) {
+      if(array[i]) {
+        res.push(array[i])
+      }
+    }
+    return res
+  },
+
+concat: function concat(array,[values]) {
+  let res = [];
+  res.push(array);
+  res.push([values])
+  return res;
+},
+
+drop: function drop(array, number = 1) {
+  if (array.length < number) {
+    return []
   }
+  for (let i = 0; i < number; i++) {
+    array.shift()
+  }
+  return array
+},
+
+dropRight: function dropRight(array, number = 1) {
+  if (array.length < number) {
+    return []
+  }
+  for (let i = 0; i < number; i++) {
+    array.pop()
+  }
+  return array
+}
 
 
 
